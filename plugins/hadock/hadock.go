3121dffa998f09c5a1cb8c42bc23082b8343d859
midbel <brun.nicolas21@gmail.com>
2019-10-09 09:29:34 +0200

    update skeleton for hadock plugin

--- plugins/hadock/hadock.go
+++ plugins/hadock/hadock.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"hash"
+	"time"
+
 	"github.com/busoc/prospect"
+	"github.com/midbel/glob"
 )
 
 const (
@@ -10,18 +14,51 @@ const (
 	fileUPI      = "file.upi"
 	fileInstance = "file.instance"
 	fileMode     = "file.mode"
+	fileFCC      = "file.fcc"
+	fileWidth    = "file.pixels.x"
+	fileHeight   = "file.pixels.y"
 )
 
 type module struct {
-	dir     string
-	pattern string
+	cfg prospect.Config
+
+	buf    []byte
+	digest hash.Hash
+	source *glob.Globber
 }
 
-func New() prospect.Module {
-	return nil
+func New(cfg prospect.Config) prospect.Module {
+	m := module{
+		cfg:    cfg,
+		buf:    make([]byte, 8<<20),
+		digest: cfg.Hash(),
+		source: glob.New("", cfg.Location),
+	}
+	return m
 }
 
 func (m module) Process() (prospect.FileInfo, error) {
-	var i prospect.FileInfo
-	return i, nil
+	var file string
+	for {
+		file = m.source.Glob()
+		if file == "" {
+			return prospect.FileInfo{}, prospect.ErrDone
+		}
+		if filepath.Ext(file) == ".xml" {
+			continue
+		}
+	}
+	i, err := m.process(file)
+	if err == nil {
+
+	}
+	return i, err
+}
+
+func parseFilename(file string) ([]prospect.Parameter, error) {
+	return nil, nil
+}
+
+func timeFromFile(file string) time.Time {
+	return time.Now()
 }
