a57488835b5d2f6dbdc148bd9e65796110e15c4e
nico <brun.nicolas21@gmail.com>
2019-10-14 15:03:44 +0200

    include metadata for uplinks listing

--- plugins/filup/filup.go
+++ plugins/filup/filup.go
@@ -1,25 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"hash"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/busoc/prospect"
 )
 
 const (
-	fileSize     = "file.size"
-	fileMD5      = "file.md5"
-	fileOriginal = "file.filename.original"
-	fileUplink   = "file.filename.uplink"
-	fileMMU      = "file.filename.mmu"
-	fileUpTime   = "file.time.uplink"
-	fileFerTime  = "file.time.transfer"
-	fileSource   = "file.source"
+	fileSize      = "file.size"
+	fileMD5       = "file.md5"
+	fileOriginal  = "file.filename.original"
+	fileUplink    = "file.filename.uplink"
+	fileMMU       = "file.filename.mmu"
+	fileUpTime    = "file.time.uplink"
+	fileFerTime   = "file.time.transfer"
+	fileSource    = "file.source"
+	fileRecords   = "file.numrec"
+	fileReference = "file.reference"
 )
 
 type module struct {
@@ -29,8 +33,8 @@ type module struct {
 	reader *csv.Reader
 	digest hash.Hash
 
-	canReadNext bool
-	sources     map[string]struct{}
+	next    []string
+	sources map[string]struct{}
 }
 
 func New(cfg prospect.Config) (prospect.Module, error) {
@@ -39,12 +43,11 @@ func New(cfg prospect.Config) (prospect.Module, error) {
 		return nil, err
 	}
 	m := &module{
-		cfg:         cfg,
-		digest:      cfg.Hash(),
-		Closer:      r,
-		reader:      csv.NewReader(r),
-		sources:     make(map[string]struct{}),
-		canReadNext: true,
+		cfg:     cfg,
+		digest:  cfg.Hash(),
+		Closer:  r,
+		reader:  csv.NewReader(r),
+		sources: make(map[string]struct{}),
 	}
 	m.reader.ReuseRecord = true
 	m.reader.FieldsPerRecord = 9
@@ -57,15 +60,30 @@ func (m *module) String() string {
 }
 
 func (m *module) Process() (prospect.FileInfo, error) {
-  row, err := m.reader.Read()
-	switch err {
-	case nil:
-	case io.EOF:
-		m.Closer.Close()
-		return prospect.FileInfo{}, prospect.ErrDone
-	default:
-		return prospect.FileInfo{}, err
+	var (
+		row []string
+		err error
+	)
+
+	if m.next == nil {
+		row, err = m.reader.Read()
+		switch err {
+		case nil:
+		case io.EOF:
+			m.Closer.Close()
+			return prospect.FileInfo{}, prospect.ErrDone
+		default:
+			return prospect.FileInfo{}, err
+		}
+		if _, ok := m.sources[row[0]]; !ok {
+			m.sources[row[0]] = struct{}{}
+			m.next = row
+		}
+		return m.processListing(row[0])
+	} else {
+		row, m.next = m.next, nil
 	}
+
 	i, err := m.process(row)
 	switch err {
 	case nil:
@@ -78,6 +96,56 @@ func (m *module) Process() (prospect.FileInfo, error) {
 	return i, err
 }
 
+func (m *module) processListing(file string) (prospect.FileInfo, error) {
+	var i prospect.FileInfo
+
+	r, err := os.Open(file)
+	if err != nil {
+		return i, err
+	}
+	defer func() {
+		r.Close()
+		m.digest.Reset()
+	}()
+
+	scan := bufio.NewScanner(io.TeeReader(r, m.digest))
+
+	var (
+		refs []string
+		size int
+	)
+	for scan.Scan() {
+		row := scan.Text()
+		if strings.HasPrefix(row, "Filename:") {
+			refs = append(refs, strings.TrimSpace(strings.TrimPrefix(row, "Filename:")))
+		}
+		size += len(row)
+	}
+	if err := scan.Err(); err != nil {
+		return i, err
+	}
+
+	s, err := r.Stat()
+	if err != nil {
+		return i, err
+	}
+	i.Parameters = []prospect.Parameter{
+		newParameter(fileSize, fmt.Sprintf("%d", size)),
+		newParameter(fileRecords, fmt.Sprintf("%d", len(refs))),
+	}
+	dir := filepath.Dir(file)
+	for _, r := range refs {
+		i.Parameters = append(i.Parameters, newParameter(fileReference, filepath.Join(dir, r)))
+	}
+
+	i.AcqTime = s.ModTime().UTC()
+	i.ModTime = s.ModTime().UTC()
+	i.Sum = fmt.Sprintf("%x", m.digest.Sum(nil))
+	i.File = r.Name()
+
+	return i, err
+}
+
 func (m *module) process(row []string) (prospect.FileInfo, error) {
 	var i prospect.FileInfo
 
@@ -86,7 +154,6 @@ func (m *module) process(row []string) (prospect.FileInfo, error) {
 	if err != nil {
 		return i, prospect.ErrSkip
 	}
-	fmt.Println(r.Name())
 	defer func() {
 		r.Close()
 		m.digest.Reset()
